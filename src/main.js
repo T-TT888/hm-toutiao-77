4a03198667961e221a050f0bd6fc19e898f2884a
TTT <1447423901@qq.com>
2019-08-02 05:07:49 +0800

    login-仅视图样式界面完成50%

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import router from '@/router'
+import '@/style/index.less'
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 new Vue({
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <div class="container">
-      <p>哈哈哈，我是login页面</p>
+      <el-card class="box-card">
+        <img src="../../assets/images/logo_index.png" alt />
+      </el-card>
     </div>
   </div>
 </template>
@@ -10,5 +12,24 @@
 export default {}
 </script>
 
-<style lang="less">
+<style scoped lang="less">
+.container {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: url(../../assets/images/login_bg.jpg) no-repeat center / cover;
+}
+.box-card {
+  width: 350px;
+  height: 300px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -60%);
+}
+.box-card img {
+  display: block;
+  width: 200px;
+  margin: 0 auto;
+}
 </style>
