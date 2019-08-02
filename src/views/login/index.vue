54b69edd2f2b810bb1a38750c6c8893941813500
TTT <1447423901@qq.com>
2019-08-02 16:23:11 +0800

    登陆表单绘制完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,13 +3,41 @@
     <div class="container">
       <el-card class="box-card">
         <img src="../../assets/images/logo_index.png" alt />
+        <el-form :model="loginForm">
+          <el-form-item>
+            <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-input
+              v-model="loginForm.code"
+              placeholder="请输入验证码"
+              style="width:237px; margin-right:10px"
+            ></el-input>
+            <el-button>发送验证码</el-button>
+          </el-form-item>
+          <el-form-item>
+            <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" style="width:100%;">主要按钮</el-button>
+          </el-form-item>
+        </el-form>
       </el-card>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      loginForm: {
+        mobile: '',
+        code: ''
+      }
+    }
+  }
+}
 </script>
 
 <style scoped lang="less">
@@ -20,8 +48,8 @@ export default {}
   background: url(../../assets/images/login_bg.jpg) no-repeat center / cover;
 }
 .box-card {
-  width: 350px;
-  height: 300px;
+  width: 400px;
+  height: 350px;
   position: absolute;
   left: 50%;
   top: 50%;
@@ -30,6 +58,6 @@ export default {}
 .box-card img {
   display: block;
   width: 200px;
-  margin: 0 auto;
+  margin: 0 auto 20px;
 }
 </style>
