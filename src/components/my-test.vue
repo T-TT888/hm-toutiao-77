788913c2b00907d3f7cbd30c304b2ec31693bfe8
TTT <1447423901@qq.com>
2019-08-06 00:37:56 +0800

    views/article 内容管理页面布局完成80%

---
+++ src/components/my-test.vue
@@ -0,0 +1,18 @@
+<!--  -->
+<template>
+  <div class='container'>
+       <div class="header">header</div>
+       <div class="content">
+           <slot></slot>
+       </div>
+       <div class="footer">footer</div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang='less'>
+
+</style>
--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -1,17 +1,86 @@
 <!--  -->
 <template>
-  <div class='container'></div>
+  <div class="container">
+    <el-card>
+      <div slot="header" >
+         <el-breadcrumb separator-class="el-icon-arrow-right">
+         <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+         <el-breadcrumb-item>内容管理</el-breadcrumb-item>
+         </el-breadcrumb>
+      </div>
+       <el-form label-width="80px" size="small">
+         <el-form-item label="状态">
+            <el-radio-group v-model="reqParams.status">
+              <el-radio :label="null">全部</el-radio>
+              <el-radio :label="0">草稿</el-radio>
+              <el-radio :label="1">待审核</el-radio>
+              <el-radio :label="2">审核通过</el-radio>
+              <el-radio :label="3">审核失败</el-radio>
+            </el-radio-group>
+         </el-form-item>
+         <el-form-item label="频道" >
+            <el-select v-model="reqParams.channel_id" placeholder="请选择">
+                <el-option
+                   v-for="item in channelOptions"
+                   :key="item.value"
+                   :label="item.label"
+                   :value="item.value">
+                </el-option>
+             </el-select>
+         </el-form-item>
+         <el-form-item label="日期">
+           <el-date-picker
+              v-model="dataArr"
+              type="daterange"
+              range-separator="至"
+              start-placeholder="开始日期"
+              end-placeholder="结束日期">
+           </el-date-picker>
+         </el-form-item>
+         <el-form-item>
+           <el-button type="primary">筛选</el-button>
+         </el-form-item>
+       </el-form>
+    </el-card>
+    <!-- <el-card>
+      <div slot="header">
+          根据数据筛选条件共查询到0条结果
+      </div>
+      <el-table :data="articles">
+        <el-table-column label="封面" prop='img'></el-table-column>
+      </el-table>
+      <div style="text-align:center;margin-top:20px">
+        <el-pagination
+          background
+          layout="prev, pager, next"
+          :total="1000">
+        </el-pagination>
+      </div>
+    </el-card> -->
+   <my-test>content</my-test>
+  </div>
 </template>
 
 <script>
+import MyTest from '@/components/my-test'
 export default {
-  created () {
-    this.$http.get('http://ttapi.research.itcast.cn/mp/v1_0/articles').then(res => console.log(res.data)
-    )
+  components: { MyTest },
+  data () {
+    return {
+      reqParams: {
+        status: null,
+        channel_id: null
+      },
+      channelOptions: [{ value: 1, label: 'json' }],
+      dataArr: []
+      // articles: [{ img: '真好' }]
+    }
   }
 }
 </script>
 
 <style scoped lang='less'>
-
+.el-card{
+  margin-bottom: 20px;
+}
 </style>
