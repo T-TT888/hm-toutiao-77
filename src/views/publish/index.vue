1f667fcc62db15e330039ac3ef07ec26d750485e
TTT <1447423901@qq.com>
2019-09-18 16:05:17 +0800

    发布文章完成

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -3,7 +3,7 @@
   <div class='publish-container'>
       <el-card>
          <div slot="header">
-            <my-bread>发布文章</my-bread>
+            <my-bread>{{articleId?'修改文章': '发布文章'}}</my-bread>
          </div>
          <el-form label-width="100px" >
              <el-form-item label="标题">
@@ -13,22 +13,32 @@
                  <quill-editor v-model="articleForm.content" :options="editorOption"></quill-editor>
              </el-form-item>
              <el-form-item label="封面">
-                   <el-radio-group v-model="articleForm.cover.type">
+                   <el-radio-group v-model="articleForm.cover.type" @change="changeType">
                         <el-radio :label="1">单图</el-radio>
                         <el-radio :label="3">三图</el-radio>
                         <el-radio :label="0">无图</el-radio>
                         <el-radio :label="-1">自动</el-radio>
                     </el-radio-group>
-                    <div class="img-btn">
-                        <img src="../../assets/images/default.png" alt="">
-                    </div>
+                    <!-- 图片组件 -->
+                   <div v-if="articleForm.cover.type ===1">
+                     <my-image v-model="articleForm.cover.images[0]"></my-image>
+                   </div>
+                   <div v-if="articleForm.cover.type ===3">
+                     <my-image v-model="articleForm.cover.images[0]"></my-image>
+                     <my-image v-model="articleForm.cover.images[1]"></my-image>
+                     <my-image v-model="articleForm.cover.images[2]"></my-image>
+                   </div>
              </el-form-item>
              <el-form-item label="频道">
                  <my-channel v-model="articleForm.channel_id"  ></my-channel>
              </el-form-item>
-             <el-form-item>
-                 <el-button type="primary">发表</el-button>
-                 <el-button >存入草稿</el-button>
+             <el-form-item v-if="!articleId">
+                 <el-button type="primary" @click="submit(false)">发布</el-button>
+                 <el-button @click="submit(true)">存入草稿</el-button>
+             </el-form-item>
+             <el-form-item v-else>
+                 <el-button type="success" @click="update(false)">修改</el-button>
+                 <el-button @click="update(true)">存入草稿</el-button>
              </el-form-item>
          </el-form>
       </el-card>
@@ -39,7 +49,6 @@
 import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
-
 import { quillEditor } from 'vue-quill-editor'
 export default {
   components: {
@@ -54,7 +63,7 @@ export default {
           type: 1,
           images: []
         },
-        channel_id: []
+        channel_id: null
       },
       editorOption: {
         placeholder: '',
@@ -68,21 +77,55 @@ export default {
             ['image']
           ]
         }
+      },
+      // 文章id
+      articleId: null
+    }
+  },
+  watch: {
+    // 不仅仅是data中的数据才可以监听
+    $route () {
+      if (!this.$route.query.id) {
+        this.articleId = null
+        this.articleForm = {
+          title: null,
+          content: null,
+          cover: {
+            type: 1,
+            images: []
+          },
+          channel_id: null
+        }
       }
+    }
+  },
+  created () {
+    this.articleId = this.$route.query.id
+    // 判断如果有id 修改文章 获取当前文章的数据
+    if (this.articleId) {
+      this.getArticle()
+    }
+  },
+  methods: {
+    async getArticle () {
+      const { data: { data } } = await this.$http.get('articles/' + this.articleId)
+      this.articleForm = data
+    },
+    changeType () {
+      this.articleForm.cover.images = []
+    },
+    async submit (draft) {
+      await this.$http.post(`articles?draft=${draft}`, this.articleForm)
+      this.$message.success(draft ? '文章存入草稿成功' : '发布文章成功')
+      this.$router.push('/article')
+    },
+    async update (draft) {
+      await this.$http.put(`articles/${this.articleId}?draft=${draft}`, this.articleForm)
+      this.$message.success(draft ? '修改存入草稿成功' : '修改文章成功')
+      this.$router.push('/article')
     }
   }
 }
 </script>
 
-<style scoped lang='less'>
-.img-btn{
-    width: 160px;
-    height: 160px;
-    border: 1px dashed #ddd;
-     img{
-        width: 100%;
-        height: 100%;
-        display: block;
-    }
-}
-</style>
+<style scoped lang='less'></style>
